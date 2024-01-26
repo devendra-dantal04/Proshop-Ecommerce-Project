51e75bfae1c03f019e2f36d5c725d7be03894a62
Devendra Dantal <devendra.dantal04@gmail.com>
2024-01-27 03:17:14 +0530

    Routes and Error Handlers are introduces with the Database Setup

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 require('dotenv').config();
 const express = require('express');
 const products = require('./data/products');
+const colors = require("colors");
+const connectDB = require("./config/db");
+const productRouter = require("./routes/productRoutes");
+const { notFound, errorHandler } = require("./middleware/errorHandlers");
 
 const app = express();
 
+connectDB();
 
-app.get('/', (req, res) => {
-    res.send("API Running....");
-})
+app.get("/", (req, res) => {
+  res.send("API Running....");
+});
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
+app.use("/api/products", productRouter);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
-
-    res.json(product);
-})
-
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-
 app.listen(PORT, () => {
-    console.log(`Jay Shree Krishna
+  console.log(
+    `Jay Shree Krishna
 You will found your way on http://localhost:${PORT}
-You are in ${process.env.NODE_ENV} mode`);
-})
+You are in ${process.env.NODE_ENV} mode`.yellow.bold
+  );
+});
